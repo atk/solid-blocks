730d5a139af3731e2b519a6ff2b3ef0585dac03d
Alex Lohr <alex.lohr@logmein.com>
2021-07-23 10:14:54 +0200

    feat: icon button variant

--- src/blocks/button.tsx
+++ src/blocks/button.tsx
@@ -3,7 +3,7 @@ import { Component, JSX, splitProps } from "solid-js";
 import "./button.css";
 
 export type ButtonProps = JSX.HTMLAttributes<HTMLButtonElement> & {
-  variant?: "primary" | "secondary" | "link";
+  variant?: "primary" | "secondary" | "link" | "icon";
 };
 
 export const Button: Component<ButtonProps> = (props) => {
