b12d4f312b47613690ec38d29eeeeeab3c96d91d
Alex Lohr <alex.lohr@logmein.com>
2021-08-02 11:56:24 +0200

    documentation wip

--- src/blocks/spinner.docs.tsx
+++ src/blocks/spinner.docs.tsx
@@ -1,7 +1,13 @@
+import { Spinner } from "./spinner"
+
 export const SpinnerDocs = () => {
   return <>
     <h2 id="spinner-docs">Spinner</h2>
     <p>The spinner component shows a simple loading spinner.</p>
+    <div class="example">
+      <Spinner />
+    </div>
+    <pre>{`<Spinner />`}</pre>
     <hr/>
   </>
 }
