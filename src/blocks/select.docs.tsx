77571b56aa0969cf498dc225ffaa74827e997334
Alex Lohr <alex.lohr@logmein.com>
2021-08-02 11:53:38 +0200

    documentation wip
    select improvements

--- src/blocks/select.docs.tsx
+++ src/blocks/select.docs.tsx
@@ -1,7 +1,46 @@
+import { createSignal } from "solid-js"
+import { Checkbox } from "./checkbox";
+import { Select } from "./select";
+
 export const SelectDocs = () => {
+  const [vertical, setVertical] = createSignal(false);
+  const [events, setEvents] = createSignal('');
   return <>
     <h2 id="select-docs">Select</h2>
-    <p>The select component is a form select component. Since it hides its content most of the time, it should be used sparingly.</p>
+    <p>The select component is a form select component. Since it hides its content most of the time, it should be used sparingly; consider using a radio group instead. It takes normal &lt;option&gt; tags as child elements.</p>
+    <h3>Properties</h3>
+    <pre>
+      {`
+SelectProps {
+  aria-orientation?: 'horizontal' | 'vertical'
+  label: JSX.Element
+  onchange?: (value: string) => void
+  value?: string
+}`}
+    </pre>
+    <dl>
+      <dt>aria-orientation</dt>
+      <dd>if set to 'vertical', the label will be above the select field instead of to its left</dd>
+      <dt>label</dt>
+      <dd>the description of the select field</dd>
+      <dt>onchange</dt>
+      <dd>a convenience handler for changes that will receive the value of the selected option tag</dd>
+    </dl>
+    <Checkbox onchange={setVertical}>aria-orientation="vertical"</Checkbox>
+    <pre data-title="onchange-events">{events()}</pre>
+    <div class="example">
+      <Select
+        label="framework"
+        aria-orientation={vertical() ? 'vertical' : 'horizontal'}
+        onchange={(value) => setEvents(e => `onchange("${value}")\n${e}`)}
+      >
+        <option value="solid">SolidJS</option>
+        <option value="react">React</option>
+        <option value="vue">Vue</option>
+        <option value="angular">Angular</option>
+        <option value="svelte">Svelte</option>
+      </Select>
+    </div>
     <hr/>
   </>
 }
--- src/blocks/select.tsx
+++ src/blocks/select.tsx
@@ -2,14 +2,14 @@ import { Component, JSX, splitProps, onMount } from "solid-js";
 
 import "./select.css";
 
-export type SelectProps = JSX.SelectHTMLAttributes<HTMLSelectElement> &
+export type SelectProps = Omit<JSX.SelectHTMLAttributes<HTMLSelectElement>, 'onchange'> &
   { label: JSX.Element; onchange?: (value: string) => void; };
 
 export const Select: Component<SelectProps> = (props) => {
-  const [local, fieldProps] = splitProps(props, ['label', 'onchange'])
+  const [local, fieldProps] = splitProps(props, ['label', 'onchange', 'aria-orientation'])
   let field;
 
-  return <label class="sb-select">
+  return <label class="sb-select" aria-orientation={local['aria-orientation']}>
     <span class="sb-select-label">{local.label}</span>
     <select
       ref={field}
